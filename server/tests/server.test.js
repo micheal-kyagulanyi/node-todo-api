c7cf5fd8b36a97dd8d693a554436a50aa447da0a
Michael Kyagulanyi <micheal.kyagulanyi@gmail.com>
2019-11-27 12:53:19 -0600

    Added GET /todos test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,11 +4,15 @@ const request = require('supertest');
 var {app} = require('./../server');
 var {Todo} = require('./../models/todo');
 
+const todos = [
+    {text : "First text to do"},
+    {text : "Second text to do"}
+]
 
 beforeEach((done) => {
     Todo.remove({}).then(() => {
-        done();
-    });
+       return Todo.insertMany(todos)
+    }).then(() => done());
 })
 
 describe('POST /todos', () => {
@@ -27,7 +31,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -47,9 +51,22 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
         });
     });
 });
+
+
+describe('GET /todos', () => {
+    it('Should get todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res)=> {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    })
+})
