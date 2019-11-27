479e5b29eb4c2b4de7fa83485c464d6d687b83b4
Michael Kyagulanyi <micheal.kyagulanyi@gmail.com>
2019-11-27 09:57:39 -0600

    Added POST todos and refactored mangoose

---
+++ server/server.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var t = new Todo(
+        {
+            text : req.body.text
+        }
+    );
+
+    t.save().then(
+        (doc) => {
+            res.send(doc);
+        },
+        (err) => {
+            res.status(400).send(err);
+        }
+    )
+});
+
+
+app.listen(3000, () => {
+    console.log('App running on port 3000');
+});
+
+
+
+
+
+
+
+
+
+
